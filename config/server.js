8899162c56b866061bac98623a0513a840dd38f8
Carlos Barrios <juanbarrios045@gmail.com>
2020-10-14 16:27:52 -0400

    Master - Fix server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: `${process.env.HOST || "0.0.0.0"}`,
-  port: `${process.env.PORT || 1337}`,
+  host: process.env.HOST,
+  port: process.env.PORT,
   admin: {
     auth: {
       secret: env("JWT", "b1e49cc85b0c26249fdfcedb42220ed3"),
